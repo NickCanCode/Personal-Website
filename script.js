40c2cf09cdc62e403d0cb993a7ee45fbaebe3136
NickCanCode <120327305+NickCanCode@users.noreply.github.com>
2024-08-07 19:19:23 +0000

    get half way through updating to node. (not done, just need to move from replit to vscode)

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-# or with npm
-npm install @statsfm/spotify.js
-import { SpotifyAPI } from '@statsfm/spotify.js';
+import express from "express";
+const app = express();
+const port = 3000;
 
-const api = new SpotifyAPI({
-  clientCredentials: {
-    clientId: '',
-    clientSecret: '',
-  },
-  accessToken: '', // optional, required for private user data
-  refreshToken: '', // optional, required for private user data and automatic token refreshing
+app.get("/test", (req, res) => {
+  res.send("hello, world!");
+
 });
 
-const tracks = await api.tracks
-  .list([
-    '2cc8Sw1OnCuA5bV8nqWqpE',
-    '4a8pP5X2lxwU5aprY44jLn',
-    '5lIFsEWj9IjNEALbHnPosE',
-    '4S4RWAA749dKyJQ5CiKEBJ',
-    '4ZtFanR9U6ndgddUvNcjcG',
-  ])
-  .then((tracks) => {
-    tracks.forEach((track) => {
-      console.log(`${track.name} - ${track.artists[0].name}`);
-    });
-  })
-  .catch(console.error);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
