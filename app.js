3b501ae486bd96f48c09c6cdcb5cf185b4b54496
NickCanCode <nickissocool6@gmail.com>
2024-08-07 21:10:40 -0400

    Add database and submit functionality to the website

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import mongoose from "mongoose";
 
 //create the __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -8,6 +9,28 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
+
+//use mongodb
+mongoose.connect("mongodb://localhost:27017");
+
+//define blog model
+const Blog = mongoose.model('Blog', new mongoose.Schema({
+  title: String,
+  content: String
+}));
+
+//handle form submission
+app.use(express.urlencoded({ extended: true }));
+app.post('/blog', async (req, res) => {
+  const { title, content } = req.body;
+  try {
+    const newPost = new Blog({ title, content });
+    await newPost.save();
+    res.redirect(`/blog/${newPost._id}`);
+  } catch {
+    res.status(400).send("Error creating blog post D:")
+  }
+});
 
 //adds EJS
 app.set('view engine', 'ejs'); // Sets EJS as template enginge
@@ -34,10 +57,25 @@ app.get("/blog", (req, res) => {
 });
 
 //blog posts
-app.get("/blog/:post", (req, res) => {
-  const post = req.params.post; // this gets the blog post name from the URL
-  res.render('blog', { post: post, content: 'this is the content' }); // renders blog.ejs with the post name
-})
+app.get("/blog/:postId", async (req, res) => {
+  try {
+  const postId = req.params.postId; // this gets the blog postID / name from the URL
+  const post = await Blog.findById(postId); // makes the post from the schematic identified by the post id
+  if (post) {
+    res.render('blog', { title: post.title, content: post.content });
+  } else {
+    res.status(404).send('Post not found');
+  } 
+} 
+catch (error) {
+  res.status(500).send("SERVER ERROR D:");
+}
+});
+
+//new post page
+app.get("/new-post", (req, res) => {
+res.sendFile(path.join(__dirname, 'public', 'newpost.html'));
+});
 
 //contact page
 app.get("/contact", (req, res) => {
